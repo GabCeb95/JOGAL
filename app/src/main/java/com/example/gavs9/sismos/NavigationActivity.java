0679209ccf6bd0cbbb5f409b4eb705e2e0e951f9
gavs9 <gavs272@gmail.com>
2016-06-04 14:54:57 -0600

    Cambios WS

--- app/src/main/java/com/example/gavs9/sismos/NavigationActivity.java
+++ app/src/main/java/com/example/gavs9/sismos/NavigationActivity.java
@@ -1,11 +1,13 @@
 package com.example.gavs9.sismos;
 
+import android.app.AlertDialog;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,27 +15,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NavigationActivity extends AppCompatActivity
@@ -41,6 +40,9 @@ public class NavigationActivity extends AppCompatActivity
 
     ObtenerWebService hiloconexion;
     private GoogleMap mMap;
+    AlertDialog alert = null;
+    private LocationManager locManager;
+    private LocationListener locListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,8 +76,6 @@ public class NavigationActivity extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
 
     }
 
@@ -139,7 +139,8 @@ public class NavigationActivity extends AppCompatActivity
 
         // Comienza a crear los marcadores desde Geonames
         hiloconexion = new ObtenerWebService();
-        hiloconexion.execute();
+        //hiloconexion.execute("paises");
+        hiloconexion.execute("sismos");
 
     }
 
@@ -148,52 +149,62 @@ public class NavigationActivity extends AppCompatActivity
 
         @Override
         protected String doInBackground(String... params) {
-
-            String cadena = "http://api.geonames.org/earthquakesJSON?formatted=true&north=11.216819&south=8.032975&east=-82.555992&west=-85.950623&username=gabceb95&style=full";
-            //http://api.geonames.org/countryInfoJSON?formatted=true&lang=es&username=gabceb95&style=full";
-
-
             String devuelve = "";
-
-            URL url = null; // Url de donde queremos obtener información
+            String valor = params[0];
             try {
-                url = new URL(cadena);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
-                        " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
-                //connection.setHeader("content-type", "application/json");
+                if(valor == "sismos") {
 
-                int respuesta = connection.getResponseCode();
-                StringBuilder result = new StringBuilder();
+                    String cadena = "http://api.geonames.org/earthquakesJSON?formatted=true&north=11.216819&south=8.032975&east=-82.555992&west=-85.950623&username=gabceb95&style=full";
+                    URL url = null; // Url de donde queremos obtener información
+                    url = new URL(cadena);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
+                           " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
+                   //connection.setHeader("content-type", "application/json");
+                   int respuesta = connection.getResponseCode();
+                   StringBuilder result = new StringBuilder();
 
-                if (respuesta == HttpURLConnection.HTTP_OK){
+                   if (respuesta == HttpURLConnection.HTTP_OK ) {
+                       InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+                       BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+                       String line = "";
+                       while ((line = reader.readLine()) != null) {
 
+                           if (line != null)
+                               result.append(line);
+                       }
 
-                    InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+                       devuelve = (result.toString());
+                   }
+               }else{
+                    String cadena = "http://api.geonames.org/countryInfoJSON?formatted=true&lang=es&username=gabceb95&style=full";
+                    URL url = null; // Url de donde queremos obtener información
 
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+                    url = new URL(cadena);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
+                            " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
+                    //connection.setHeader("content-type", "application/json");
+                    int respuesta = connection.getResponseCode();
+                    StringBuilder result = new StringBuilder();
 
-                    // El siguiente proceso lo hago porque el JSONOBject necesita un String y tengo
-                    // que tranformar el BufferedReader a String. Esto lo hago a traves de un
-                    // StringBuilder.
+                    if (respuesta == HttpURLConnection.HTTP_OK ) {
+                        InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+                        String line = "";
+                        while ((line = reader.readLine()) != null) {
 
-                    String line;
-                    while ((line = reader.readLine()) != null) {
+                            if (line != null)
+                                result.append(line);
+                        }
 
-                        result.append(line);
+                        devuelve = (result.toString());
                     }
-
-                    devuelve = result.toString();
-                }
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch(Exception e) {
-                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+               }
+           }catch (Exception e) {
+                   Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
+
             return devuelve;
         }
 
@@ -210,13 +221,17 @@ public class NavigationActivity extends AppCompatActivity
                 JSONObject respuestaJSON = new JSONObject(aVoid);
                 //Accedemos al vector de resultados
                 JSONArray resultJSON = respuestaJSON.getJSONArray("earthquakes");   // earthquakes es el nombre del campo en el JSON
-
+                //JSONArray resultJSON2 = respuestaJSON.getJSONArray("geonames");
 
                 for (int i = 0; i < resultJSON.length(); i++) {
                     System.out.println(resultJSON.getJSONObject(i).getString("datetime"));
                     JSONObject obj = resultJSON.getJSONObject(i);
                     LatLng pos = new LatLng(obj.getDouble("lat"), obj.getDouble("lng"));
-                    mMap.addMarker(new MarkerOptions().position(pos).title(obj.getString("datetime")));
+                    mMap.addMarker(new MarkerOptions()
+                            .position(pos)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.sismo))
+                            .anchor(0.0f, 1.0f)
+                            .title(obj.getString("datetime")));
                 }
             } catch(Exception e)
             {
@@ -236,5 +251,10 @@ public class NavigationActivity extends AppCompatActivity
         }
     }
 
+
+
+    // ******************************* GPS
+
+    public void Mensaje(String msg){Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();};
 
 }
